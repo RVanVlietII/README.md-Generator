ddb4de17cde9da74afa8710cfb251886d0e9cb45
RVanVlietII <rcvanvlietii@gmail.com>
2023-10-10 15:56:24 -0400

    cleaned deleted code or commented out code

--- Develop/utils/generateFile.js
+++ Develop/utils/generateFile.js
@@ -1,23 +1,6 @@
 const fs = require('fs');
 
 
-// const writeFile = fileContent => {
-//     return new Promise((resolve, reject) => {
-//         fs.writeFile('./Develop/created', fileContent, err => {
-//             if (err) {
-//                 reject(err);
-//                 return;
-//             }
-        
-//         resolve({
-//             ok: true,
-//             message: 'Your new README.md file is now available!'
-//         });
-//         });
-//     });
-// };
-
-
 function generateFile(filePath, content) {
     fs.writeFile('./README.md-generator/created/README.md', content, err => {
       if (err) {
